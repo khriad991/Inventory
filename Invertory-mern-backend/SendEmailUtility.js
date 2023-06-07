9fb431104a27378199365edb7c861a5e7c2bcb77
Kamrul Hasan <63355519+khriad991@users.noreply.github.com>
2023-06-07 13:01:55 +0600

    2nd commit something change in impotent

---
+++ Invertory-mern-backend/SendEmailUtility.js
@@ -0,0 +1,25 @@
+const nodeMailer = require("nodemailer");
+
+const SendEmailUtility = async (EmailTo , EmailText, EmailSubject )=>{
+    let transporter = nodeMailer.createTransport({
+        host:"mail.teamrabbil.com",
+        port:25,
+        secure:false,
+        auth:{
+            user:'info@teamrabbil.com',
+            pass:'~sR4[bhaC[Qs'
+        },tls:{
+            rejectUnauthorized:false
+        },
+    })
+
+    let mailOptions = {
+        from:'Inventory <info@teamrabbil.com>',
+        to:EmailTo,
+        subject:EmailSubject,
+        text:EmailText
+    }
+    return await  transporter.sendMail(mailOptions)
+}
+
+module.exports=SendEmailUtility;
