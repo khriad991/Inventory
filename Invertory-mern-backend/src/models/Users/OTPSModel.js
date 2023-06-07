e9667e11cd4c1e14215db0789b25724316e2e4d6
Kamrul Hasan <63355519+khriad991@users.noreply.github.com>
2023-06-07 17:16:12 +0600

    add OTPSModel

---
+++ Invertory-mern-backend/src/models/Users/OTPSModel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const OTPSSchema = mongoose.Schema({
+    email:{type:String},
+    otp:{type:String},
+    status:{type:Number,default:0},
+    createdDate:{type:Date, default:Date.now()}
+},{versionKey:false})
+
+const OTPSModel = mongoose.model("otps", OTPSSchema);
+
+module.exports= OTPSModel;
